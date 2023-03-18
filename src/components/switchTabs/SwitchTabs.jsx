dbd96e93f162801999881d91231e0ae172d8aa04
Parassahu2308 <sahujuparas786@gmail.com>
2023-03-18 17:37:24 +0530

    :rocket: deploy and some changes

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -18,6 +18,7 @@ const SwitchTabs = ({ data, onTabChange }) => {
       <div className="tabItems">
         {data.map((tab, index) => (
           <span
+            key={index}
             className={`tabItem ${selectedTab === index ? "active" : ""}`}
             onClick={() => activeTab(tab, index)}
           >
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-import token from "../secret";
+// import dotenv from "dotenv";
+// import token from "../secret";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 
-const TMBD_TOKEN = token;
+const TMBD_TOKEN = import.meta.env.VITE_APP_TMBD_TOKEN;
+
+// dotenv.config();
 
 const headers = {
   Authorization: "bearer " + TMBD_TOKEN,
